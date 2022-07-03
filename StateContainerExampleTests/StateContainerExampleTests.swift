6e81f5738665502aa8131aecf71462a41783d56d
Tomas Baculak <vanya.otvorij@gmail.com>
2022-07-03 11:24:57 +0200

    extended unit tests

--- StateContainerExampleTests/StateContainerExampleTests.swift
+++ StateContainerExampleTests/StateContainerExampleTests.swift
@@ -12,12 +12,17 @@ class StateContainerExampleTests: XCTestCase {
     func testState() throws {
         let router = MainRouter()
         let viewModel = MainContentViewModel(with: router)
+        let defaultValue = MainContentViewModel.defaultValue
         viewModel.viewLoaded.send()
-        XCTAssertEqual(Decimal(viewModel.outputValue), MainContentViewModel.defaultValue)
+        XCTAssertEqual(Decimal(viewModel.outputValue), defaultValue)
+        viewModel.showDetailPressed = ()
+        XCTAssertEqual(router.context.data, defaultValue)
         viewModel.manualProgress = 1.000001
+        XCTAssertEqual(Decimal(viewModel.outputValue), 0)
         viewModel.showDetailPressed = ()
         XCTAssertEqual(router.context.data, 0)
         viewModel.manualProgress = 0.99999999
+        XCTAssertEqual(Decimal(viewModel.outputValue), 0.99999999)
         viewModel.showDetailPressed = ()
         XCTAssertEqual(router.context.data, 0.99999999)
     }
